3370a3930c216523a08a4cdc5a2140071a9cc525
Somberi860 <72584568+Somberi860@users.noreply.github.com>
2022-01-19 18:51:10 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCTPzNlB4AC6vcpJKob_OPL48_f_nkiz6U",
+    authDomain: "story-telling-app-8afd7.firebaseapp.com",
+    projectId: "story-telling-app-8afd7",
+    storageBucket: "story-telling-app-8afd7.appspot.com",
+    messagingSenderId: "80516452769",
+    appId: "1:80516452769:web:58b14eb33efcf207abbbe0"
+};
